c25a24790b31085f65a11172cfb77ab6e0c64c11
siangxuan98 <45364564+siangxuan98@users.noreply.github.com>
2020-07-22 21:16:24 +0800

    Updated with my content

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,49 @@
 <template>
   <div class="flex flex-col w-screen h-full">
     <!-- Your name, profile picture and contact -->
-    <HeadWrapper 
-      name='Your Name'
-      title='Your Job Title'
-      userImgURL='Image URL Address'
-      phone='Your Contact Number'
-      email='Your Email Contact'
+    <HeadWrapper
+      name="HeIsDying"
+      title="Trying to get employed"
+      userImgURL="../img/karambit.jpg"
+      phone="016-5986921"
+      email="siangxuan569@gmail.com"
     />
     <!-- About Me -->
-    <AboutWrapper 
-      aboutMe='Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.'
-      linkedInURL='Your LinkedIn URL Address'
-      languages='Languages'
-      nationality='Your Nationality'
-      interest='Your Interest'
+    <AboutWrapper
+      aboutMe="Undergraduate --> Unemployed"
+      linkedInURL="Your LinkedIn URL Address"
+      languages="Languages"
+      nationality="Your Nationality"
+      interest="Your Interest"
     />
     <!-- Working Experience -->
     <CareerWrapper>
       <template v-slot:career>
-        <Career 
-          companyName='Company Name'
-          title='Your Job Title'
-          startDate='Start Date'
-          endDate='End Date'
-          >
+        <Career
+          companyName="Company Name"
+          title="Your Job Title"
+          startDate="Start Date"
+          endDate="End Date"
+        >
           <!-- List of what you had did in the company -->
           <template v-slot:experience>
-            <ul class='text-gray-600 list-inside list-disc mt-2 px-4'>
+            <ul class="text-gray-600 list-inside list-disc mt-2 px-4">
               <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
               <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
               <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
             </ul>
           </template>
         </Career>
-        
-        <Career 
-          companyName='Company Name'
-          title='Your Job Title'
-          startDate='Start Date'
-          endDate='End Date'
-          >
+
+        <Career
+          companyName="Company Name"
+          title="Your Job Title"
+          startDate="Start Date"
+          endDate="End Date"
+        >
           <!-- List of what you had did in the company -->
           <template v-slot:experience>
-            <ul class='text-gray-600 list-inside list-disc mt-2 px-4'>
+            <ul class="text-gray-600 list-inside list-disc mt-2 px-4">
               <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
               <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
               <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
@@ -53,37 +53,37 @@
       </template>
     </CareerWrapper>
     <!-- Achievement -->
-    <AchievementWrapper 
-      project='Number of Project'
-      client='Number of Happy Clients'
-      award='Number of Awards'
+    <AchievementWrapper
+      project="Number of Project"
+      client="Number of Happy Clients"
+      award="Number of Awards"
     />
     <!-- Portfolio -->
     <PortfolioWrapper>
-        <template v-slot:portfolio>
-          <div class='grid grid-cols-1 md:grid-cols-3'>
-            <ProjectCard 
-              imgUrl='../img/github.png'
-              name='Project Name'
-              project='Project Type'
-              projectURL='https://github.com'
-              />
+      <template v-slot:portfolio>
+        <div class="grid grid-cols-1 md:grid-cols-3">
+          <ProjectCard
+            imgUrl="../img/github.png"
+            name="Project Name"
+            project="Project Type"
+            projectURL="https://github.com"
+          />
 
-              <ProjectCard 
-              imgUrl='../img/github.png'
-              name='Project Name'
-              project='Project Type'
-              projectURL='https://github.com'
-              />
-              
-              <ProjectCard 
-              imgUrl='../img/github.png'
-              name='Project Name'
-              project='Project Type'
-              projectURL='https://github.com'
-              />
-          </div>
-        </template>
+          <ProjectCard
+            imgUrl="../img/github.png"
+            name="Project Name"
+            project="Project Type"
+            projectURL="https://github.com"
+          />
+
+          <ProjectCard
+            imgUrl="../img/github.png"
+            name="Project Name"
+            project="Project Type"
+            projectURL="https://github.com"
+          />
+        </div>
+      </template>
     </PortfolioWrapper>
   </div>
 </template>
@@ -92,8 +92,8 @@
 export default {
   head() {
     return {
-      title: 'Your Name'
-    }
+      title: "HeIsDying"
+    };
   }
-}
+};
 </script>
